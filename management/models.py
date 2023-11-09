afc1c00b130b1eab634986ea229c58e1eba56e6a
The Hieu <thehieu0814@gmail.com>
2023-11-09 19:36:44 +0700

    web

--- management/models.py
+++ management/models.py
@@ -15,9 +15,26 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     user_name = db.Column(db.String(150))
     notes = db.relationship("Note")
-
+    # Thêm quan hệ ngược với bảng Order
+    orders = db.relationship("Order", back_populates="user")
     def __init__(self, email, password, user_name):
         self.password: Any
         self.email = email
         self.password = password
         self.user_name = user_name
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(255), nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
+
+    # Tạo khóa ngoại để liên kết với bảng User
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    
+    # Tạo quan hệ với bảng User
+    user = db.relationship("User", back_populates="orders")
+
+    def __init__(self, product_name, total_price, user):
+        self.product_name = product_name
+        self.total_price = total_price
+        self.user = user
