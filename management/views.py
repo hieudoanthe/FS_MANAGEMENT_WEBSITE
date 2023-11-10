dc99d1373f79b2fa4acf836bcd2dcf094e046d64
The Hieu <thehieu0814@gmail.com>
2023-11-10 18:12:28 +0700

    web

--- management/views.py
+++ management/views.py
@@ -26,34 +26,6 @@ def shop_cart():
 @views.route('/checkout', methods=['GET', 'POST'])
 @login_required
 def shop_checkout():
-    if request.method == 'POST':
-        # Lấy giỏ hàng từ local storage
-        cart = request.json.get('cart')
-        if not cart:
-            return jsonify({'error': 'Invalid data format'}), 400
-     # Lấy thông tin sản phẩm và giá từ request hoặc từ dữ liệu gửi lên
-        product_name = request.json.get('product_name')
-        total_price = request.json.get('total_price')
-        # Tạo một đối tượng Order và thêm vào cơ sở dữ liệu
-        new_order = Order(product_name=product_name, total_price=total_price, user=current_user)
-        db.session.add(new_order)
-        db.session.flush()
-
-        # Duyệt qua các sản phẩm trong giỏ hàng và thêm vào cơ sở dữ liệu
-        for product in cart:
-            # Tạo một đối tượng Note và thêm vào cơ sở dữ liệu
-            new_note = Order(data=product['name'], order=new_order)
-            db.session.add(new_note)
-
-        # Commit để lưu thay đổi vào cơ sở dữ liệu
-        db.session.commit()
-
-        flash('Order placed successfully!', 'success')
-
-        # Gửi phản hồi về trình duyệt để xóa giỏ hàng
-        return jsonify({'success': True})
-
-    # Nếu là GET request, hiển thị trang checkout bình thường
     return render_template('checkout.html')
 
 @views.route('/contact')
